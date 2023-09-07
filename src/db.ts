ba819fcf6ec33d6514aa52b2c4a0cc959b41b7f4
Ricardo Francoli <francoliricardo@gmail.com>
2023-09-07 10:06:59 +0200

    feat(db): add sequelize port option and default port

--- src/db.ts
+++ src/db.ts
@@ -5,9 +5,16 @@ import logger from './utils/logger'
 const { database = '', username = '', password = '', host } = connection
 const dialect = getDialect()
 
+const DEFAULT_PORT = 3306
+const port =
+  connection.port && typeof connection.port === 'number'
+    ? connection.port
+    : DEFAULT_PORT
+
 const sequelize = new Sequelize(database, username, password, {
   host,
   dialect,
+  port,
   logging: false,
 })
 
